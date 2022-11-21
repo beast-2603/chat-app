590f1b91de6d951ab3b5850c13f67f77eb959b8f
beast-2603 <senapati.sohan31@gmail.com>
2022-11-21 23:23:34 +0530

    Added Dashboard Button(functioning)

---
+++ src/components/dashboard/DashboardToggle.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button, Drawer, Icon } from 'rsuite';
+import Dashboard from '.';
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
+
+const DashboardToggle = () => {
+  const { isOpen, close, open } = useModalState();
+  const is992px = useMediaQuery('(max-width: 992px)');
+
+  return (
+    <>
+      <Button block color="blue" onClick={open}>
+        <Icon icon="dashboard" /> Dashboard
+      </Button>
+      <Drawer full={is992px} show={isOpen} onHide={close} placement="left">
+        <Dashboard />
+      </Drawer>
+    </>
+  );
+};
+
+export default DashboardToggle;
