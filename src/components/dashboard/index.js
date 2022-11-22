1b16703eec3661605a07ed043bcb00f899f18610
beast-2603 <senapati.sohan31@gmail.com>
2022-11-22 14:52:19 +0530

    nickname updation

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import { Button, Drawer } from 'rsuite';
+import { Alert, Button, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import { database } from '../../misc/Firebase';
 import EditableInput from '../EditableInput';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
-  const onSave = async newData => {};
+  const onSave = async newData => {
+    const userNicknameRef = database
+      .ref(`profiles/${profile.uid}`)
+      .child('name');
+
+    try {
+      await userNicknameRef.set(newData);
+      Alert.success('Nickname has been Updated', 4000);
+    } catch (err) {
+      Alert.error(err.message, 4000);
+    }
+  };
   return (
     <>
       <Drawer.Header>
